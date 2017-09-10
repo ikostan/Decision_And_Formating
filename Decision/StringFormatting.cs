27b81fe45bfc49766bcc4f7c06fe045cdbd3473b
ikostan <superegorius@gmail.com>
2017-09-09 18:44:15 -0700

    Test 2

--- Decision/StringFormatting.cs
+++ Decision/StringFormatting.cs
@@ -42,39 +42,39 @@ namespace Decision
                 switch (DecisionsAndFormattingProgram.GetUserInput())
                 {
                     case 1:
-                       
+                        //
                         break;
 
                     case 2:
-                        
+                        //
                         break;
 
                     case 3:
-                        
+                        //
                         break;
 
                     case 4:
-                        
+                        //
                         break;
 
                     case 5:
-
+                        //
                         break;
 
                     case 6:
-
+                        //
                         break;
 
                     case 7:
-
+                        //
                         break;
 
                     case 8:
-
+                        //
                         break;
 
                     case 9:
-
+                        //
                         break;
 
                     case 10:
